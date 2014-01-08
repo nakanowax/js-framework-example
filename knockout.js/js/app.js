ba8e4435d69efac82a11de4db7772b58ff1efd5e
nakano.wax <nakano.styling.wax.hard@gmail.com>
2014-01-09 01:19:24 +0900

    knockout.js ajsx処理追加

--- knockout.js/js/app.js
+++ knockout.js/js/app.js
@@ -1,29 +1,44 @@
 // ViewModel を定義します
 var ViewModel = function(first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
+    var self = this;
+    self.firstName = ko.observable(first);
+    self.lastName = ko.observable(last);
 
-    this.fullName = ko.computed(function() {
+    self.fullName = ko.computed(function() {
         // Knockout は依存を自動的にトラッキングします。
         // fullName の評価中に firstName と lastName を呼び出すため、
         // それぞれに依存していることが検知されます。
-        return this.firstName() + " " + this.lastName();
-    }, this);
+        return self.firstName() + " " + self.lastName();
+    }, self);
 
+
+/*
     this.profile = ko.observableArray([
         { id: 1, name: 'hoge', age: 21 },
         { id: 2, name: 'boo',  age: 31 },
         { id: 3, name: 'bar',  age: 41 }
     ]);
+*/
 
-    this.inputId = ko.observable();
-    this.inputName = ko.observable();
-    this.inputAge = ko.observable();
-    this.addItem = function() {
-        this.profile.push({
-            id: this.inputId,
-            name: this.inputName,
-            age: this.inputAge
+    self.profile = ko.observableArray();
+    self.getData = function() {
+        self.profile.removeAll();
+        var url = "http://localhost:9292/get";
+        $.getJSON(url, function(data) {
+            for (var i = 0; i < data.data.length; i++) {
+                self.profile.push(data.data[i]);
+            }
+        });
+    };
+
+    self.inputId = ko.observable();
+    self.inputName = ko.observable();
+    self.inputAge = ko.observable();
+    self.addItem = function() {
+        self.profile.push({
+            id: self.inputId,
+            name: self.inputName,
+            age: self.inputAge
         });
     };
 };
