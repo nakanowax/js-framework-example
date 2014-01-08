39ee3e718fa3849077965b7b973b6fb6ed8191ef
nakanowax <nakano.styling.wax.hard@gmail.com>
2014-01-08 19:37:05 +0900

    angular.js の add処理追加

--- angular.js/js/app.js
+++ angular.js/js/app.js
@@ -1,8 +1,21 @@
 
-function profile ($scope) {
+function showView ($scope) {
     $scope.people = [
         { id: 1, name: 'hoge', age: 21 },
         { id: 2, name: 'boo',  age: 31 },
         { id: 3, name: 'bar',  age: 41 }
     ];
+
+    $scope.inputId = "";
+    $scope.inputName = "";
+    $scope.inputAge = "";
+
+    $scope.addItem = function (){
+        console.log("click!!");
+        $scope.people.push({
+            id: $scope.inputId,
+            name: $scope.inputName,
+            age: $scope.inputAge,
+        });
+    }
 }
