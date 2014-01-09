5a0654e5911b13875276f117665c82949ee47fd1
nakanowax <nakano.styling.wax.hard@gmail.com>
2014-01-09 22:56:27 +0900

    ember.js ページ遷移,API取得部分作成(とりあえず )

--- ember.js/js/app.js
+++ ember.js/js/app.js
@@ -1,9 +1,12 @@
 var App = Ember.Application.create();
 
+App.Router.map(function() {
+    this.route("aboutme");
+});
+
 App.ApplicationController = Ember.ObjectController.extend({
     actions: {
         addItem: function() {
-            console.log('clicked!!');
             this.get('content').pushObject(
                   Ember.Object.create({
                       id: $('#input-id').val(),
@@ -11,12 +14,27 @@ App.ApplicationController = Ember.ObjectController.extend({
                       age: $('#input-age').val()
                   })
             )
+        },
+        getData: function(){
+            var url = "http://localhost:9292/get";
+            $.getJSON(url, function(response) {
+                console.log(response);
+            });
+        },
+        getDataFromPost: function(){
+            var url = "http://localhost:9292/";
+            var data = {method: 'getUserData'};
+            $.post(url, JSON.stringify(data), function(response) {
+                console.log(response);
+            });
         }
     }
 });
 
+App.store = DS.Store.extend();
+
 App.ApplicationRoute = Ember.Route.extend({
-  model: function() {
+    model: function() {
     return [
         {id: 1, name: 'hoge', age: 21},
         {id: 2, name: 'boo', age: 31},
@@ -24,9 +42,20 @@ App.ApplicationRoute = Ember.Route.extend({
     ]}
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+App.PeopleView = Ember.View.extend({
+    templateName: 'people',
+});
+
+App.OtherView = Ember.View.extend({
+    templateName: 'other',
+});
+
+App.CountUpView = Ember.View.extend({
+    templateName: 'countup',
+    count: 1,
+    click: function(evt){
+        var current = this.get('count');
+        this.set('count', current + 1);
+    }
 });
 
